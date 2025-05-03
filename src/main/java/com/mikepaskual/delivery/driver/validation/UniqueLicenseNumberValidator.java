e06c14303c4e61574c1cb1eee55eb744fdb59f43
Miguel A. Pascual <mianpascual@gmail.com>
2025-05-03 13:54:06 +0200

    Fix validation license number

--- src/main/java/com/mikepaskual/delivery/driver/validation/UniqueLicenseNumberValidator.java
+++ src/main/java/com/mikepaskual/delivery/driver/validation/UniqueLicenseNumberValidator.java
@@ -1,10 +1,16 @@
 package com.mikepaskual.delivery.driver.validation;
 
+import com.mikepaskual.delivery.driver.model.Driver;
 import com.mikepaskual.delivery.driver.model.DriverRepository;
+import com.mikepaskual.delivery.user.model.User;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class UniqueLicenseNumberValidator implements ConstraintValidator<UniqueLicenseNumber, String> {
@@ -14,6 +20,14 @@ public class UniqueLicenseNumberValidator implements ConstraintValidator<UniqueL
 
     @Override
     public boolean isValid(String licenseNumber, ConstraintValidatorContext context) {
-        return !driverRepository.findByLicenseNumber(licenseNumber).isPresent();
+        Optional<Driver> driver = driverRepository.findByLicenseNumber(licenseNumber);
+        if (!driver.isPresent()) {
+            return true;
+        }
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = authentication.getPrincipal();
+        User authenticatedUser = (User) principal;
+        Long authenticatedUserId = authenticatedUser.getId();
+        return driver.get().getId().equals(authenticatedUserId);
     }
 }
