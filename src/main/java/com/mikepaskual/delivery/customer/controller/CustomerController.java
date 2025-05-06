a93d9e5def7bb8fca571a3ac3a1db4422f13fd4f
Miguel A. Pascual <mianpascual@gmail.com>
2025-05-06 23:00:52 +0200

    Delete customer operations

--- src/main/java/com/mikepaskual/delivery/customer/controller/CustomerController.java
+++ src/main/java/com/mikepaskual/delivery/customer/controller/CustomerController.java
@@ -1,64 +1,8 @@
 package com.mikepaskual.delivery.customer.controller;
 
-import com.mikepaskual.delivery.address.model.Address;
-import com.mikepaskual.delivery.address.service.AddressService;
-import com.mikepaskual.delivery.customer.model.Customer;
-import com.mikepaskual.delivery.customer.service.CustomerService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Controller
 public class CustomerController {
-
-    @Autowired
-    private final CustomerService customerService;
-    @Autowired
-    private final AddressService addressService;
-
-    public CustomerController(CustomerService customerService, AddressService addressService) {
-        this.addressService = addressService;
-        this.customerService = customerService;
-    }
-
-    @GetMapping("/customers")
-    public String showCustomers(@RequestParam(defaultValue = "0") int page,
-                                @RequestParam(defaultValue = "10") int size,
-                                @RequestParam(defaultValue = "createdAt") String sortField,
-                                @RequestParam(defaultValue = "desc") String sortDir,
-                                Model model) {
-        Sort.Direction direction = sortDir.equalsIgnoreCase("asc") ? Sort.Direction.ASC : Sort.Direction.DESC;
-        Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sortField));
-        Page<Customer> customerPage = customerService.findPaginated(pageable);
-
-        model.addAttribute("customers", customerPage.getContent());
-        model.addAttribute("currentPage", page);
-        model.addAttribute("totalPages", customerPage.getTotalPages());
-        model.addAttribute("size", size);
-        model.addAttribute("sortField", sortField);
-        model.addAttribute("sortDir", sortDir);
-        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
-
-        return "customer/customers";
-    }
-
-    @GetMapping("/customers/{id}")
-    public String editOrViewCustomer(@PathVariable Long id, Model model) {
-        Customer customer = customerService.findById(id);
-        model.addAttribute("customer", customer);
-        //model.addAttribute("addresses", addressService.findNotHiddenByCustomer(customer));
-        return "customer/customer";
-    }
 
 }
--- src/main/java/com/mikepaskual/delivery/customer/model/Customer.java
+++ src/main/java/com/mikepaskual/delivery/customer/model/Customer.java
@@ -1,13 +1,8 @@
 package com.mikepaskual.delivery.customer.model;
 
-import com.mikepaskual.delivery.address.model.Address;
-import com.mikepaskual.delivery.user.model.Gender;
 import jakarta.persistence.*;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name = "customer_entity")
@@ -16,20 +11,10 @@ public class Customer {
     @Id
     @GeneratedValue
     private Long id;
-    private String firstName;
-    private String lastName;
-    private String phone;
-    private String email;
-    @Enumerated(EnumType.STRING)
-    private Gender gender;
-    private LocalDate birthday;
     private LocalDateTime createdAt;
-    @OneToMany(fetch = FetchType.LAZY)
-    @JoinColumn(name = "customer_id")
-    private List<Address> addresses;
 
     public Customer() {
-        addresses = new ArrayList<>();
+        super();
     }
 
     public Long getId() {
@@ -40,72 +25,12 @@ public class Customer {
         this.id = id;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public List<Address> getAddresses() {
-        return addresses;
-    }
-
-    public void setAddresses(List<Address> addresses) {
-        if (addresses == null) {
-            this.addresses = new ArrayList<>();
-        } else {
-            this.addresses = new ArrayList<>(addresses);
-        }
-    }
-
     public LocalDateTime getCreatedAt() {
         return createdAt;
     }
 
     public void setCreatedAt(LocalDateTime createdAt) {
         this.createdAt = createdAt;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    public LocalDate getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(LocalDate birthday) {
-        this.birthday = birthday;
     }
 
     @SuppressWarnings("rawtypes")
@@ -117,23 +42,10 @@ public class Customer {
     public static class Builder<S extends Builder> {
 
         private Long id;
-        private String firstName;
-        private String lastName;
-        private String phone;
-        private String email;
-        private List<Address> addresses;
-        private Gender gender;
-        private LocalDate birthday;
         private LocalDateTime createdAt;
 
         public Builder() {
             super();
-        }
-
-        @SuppressWarnings("unchecked")
-        public S setBirthday(LocalDate birthday) {
-            this.birthday = birthday;
-            return (S) this;
         }
 
         @SuppressWarnings("unchecked")
@@ -143,58 +55,15 @@ public class Customer {
         }
 
         @SuppressWarnings("unchecked")
-        public S setFirstName(String firstName) {
-            this.firstName = firstName;
-            return (S) this;
-        }
-
-        @SuppressWarnings("unchecked")
-        public S setEmail(String email) {
-            this.email = email;
-            return (S) this;
-        }
-
-        @SuppressWarnings("unchecked")
-        public S setAddresses(List<Address> addresses) {
-            this.addresses = addresses;
-            return (S) this;
-        }
-
-        @SuppressWarnings("unchecked")
         public S setCreatedAt(LocalDateTime createdAt) {
             this.createdAt = createdAt;
             return (S) this;
         }
 
-        @SuppressWarnings("unchecked")
-        public S setLastName(String lastName) {
-            this.lastName = lastName;
-            return (S) this;
-        }
-
-        @SuppressWarnings("unchecked")
-        public S setPhone(String phone) {
-            this.phone = phone;
-            return (S) this;
-        }
-
-        @SuppressWarnings("unchecked")
-        public S setGender(Gender gender) {
-            this.gender = gender;
-            return (S) this;
-        }
-
         public Customer build() {
             Customer customer = new Customer();
-            customer.setAddresses(this.addresses);
-            customer.setBirthday(this.birthday);
             customer.setCreatedAt(this.createdAt);
-            customer.setEmail(this.email);
-            customer.setGender(this.gender);
             customer.setId(this.id);
-            customer.setFirstName(this.firstName);
-            customer.setLastName(this.lastName);
-            customer.setPhone(this.phone);
             return customer;
         }
     }
